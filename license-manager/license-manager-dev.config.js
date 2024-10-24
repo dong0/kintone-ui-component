962c38b198de57fef1fe24e7f4106804bcebe580
DongLin <ldong@cybozu.cn>
2024-10-24 10:10:39 +0800

    SSR-4321: Migrate the license management system from LicenseFinder to @cybozu/license-manager

--- license-manager/license-manager-dev.config.js
+++ license-manager/license-manager-dev.config.js
@@ -70,6 +70,28 @@ const OVERRIDE_LICENSES = {
 
 const OVERRIDE_LICENSES_TEXT = {
   "fs@0.0.1-security": "ISC License", // Can't find license file. https://www.npmjs.com/package/fs/v/0.0.1-security
+  "eslint-plugin-kuc-v1@1.0.0": "MIT License", //
+  "@web/test-runner@0.19.0": `MIT License
+
+Copyright (c) 2020 modern-webdev
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`
 };
 
 const config = {
